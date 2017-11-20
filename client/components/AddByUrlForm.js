f9874275053d5700f9e47f17ba9ccd15f2e036d6
Sol <sppark95@hotmail.com>
2017-11-20 11:36:28 -0500

    chore(addUrlOnNav)- fix lint issues

--- client/components/AddByUrlForm.js
+++ client/components/AddByUrlForm.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 import { Form, Button } from 'semantic-ui-react';
+
 const AddByUrlForm = () => {
   return (
     <Form>
       <h5>Save an item</h5>
       <Form.Field>
         <label>Enter Url</label>
-        <input placeholder='http://...'/>
+        <input placeholder="http://..." />
       </Form.Field>
-      <Button type='submit'>Save</Button>
+      <Button type="submit">Save</Button>
     </Form>
-    )
-}
+  );
+};
 // mapDispatch = dispatch => ({
 //   handleSubmit(evt, userId){
 //     evt.preventDefault();
-        
+
 //   }
 // })
 export default connect(null)(AddByUrlForm);
--- server/api/video.js
+++ server/api/video.js
@@ -1,15 +1,9 @@
 const router = require('express').Router()
-const {User,Video} = require('../db/models')
+const {User, Video} = require('../db/models')
 module.exports = router
 
-router.post('/')
-router.get('/', (req, res, next) => {
-  User.findAll({
-    // explicitly select only the id and email fields - even though
-    // users' passwords are encrypted, it won't help if we just
-    // send everything to anyone who asks!
-    attributes: ['id', 'email']
-  })
-    .then(users => res.json(users))
-    .catch(next)
+router.post('/', (req, res, next ) => {
+  Video.create(req.body)
+  .then(_ => res.sendStatus(201))
+  .catch(next)
 })
