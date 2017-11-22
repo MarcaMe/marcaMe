9be728761672e25626287e313156ed715fdb1649
Sol <sppark95@hotmail.com>
2017-11-22 14:03:49 -0500

    feat(editContent)- create destroy route for content

--- client/components/ContentCard.js
+++ client/components/ContentCard.js
@@ -1,44 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card, Icon, Image } from 'semantic-ui-react';
 
-const truncateDescription = des => {
-  const desArr = des.split(' ');
-  if (desArr.length > 19) return desArr.slice(0, 19).join(' ') + '...';
-  else return des;
-};
+export class ContentCard extends Component {
+  _truncateDescription(des) {
+    const desArr = des.split(' ');
+    if (desArr.length > 19) return desArr.slice(0, 19).join(' ') + '...';
+    else return des;
+  }
 
-export const ContentCard = props => {
-  return (
-    <Card
-      style={{ width: '300px', height: '350px', margin: '0.5vw' }}
-      color={props.color}
-      className="card"
-      fluid
-    >
-      <Card.Content style={{ overflow: 'hidden' }}>
-        <Card.Header>{props.story.title}</Card.Header>
-        <div
-          style={{ margin: '10px auto', overflow: 'hidden', height: '150px' }}
-        >
-          <Image
-            style={{ width: '100%', display: 'block' }}
-            fluid
-            src={props.story.imageUrl}
-          />
-        </div>
-        <Card.Meta>
-          <span className="date">{props.story.date}</span>
-        </Card.Meta>
-        <Card.Description style={{ fontSize: '1em' }} className="description">
-          {truncateDescription(props.story.description)}
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <span><Icon size="large" name="user"></Icon></span>
-        <span><Icon size="large" name="trash"></Icon></span>
-      </Card.Content>
-    </Card>
-  );
-};
+  render() {
+    const story = this.props.story;
+    return (
+      <Card
+        style={{ width: '300px', height: '350px', margin: '0.5vw' }}
+        color={this.props.color}
+        className="card"
+        fluid
+      >
+        <Card.Content style={{ overflow: 'hidden' }}>
+          <Card.Header>{story.title}</Card.Header>
+          <div
+            style={{ margin: '10px auto', overflow: 'hidden', height: '150px' }}
+          >
+            <Image
+              style={{ width: '100%', display: 'block' }}
+              fluid
+              src={story.imageUrl}
+            />
+          </div>
+          <Card.Meta>
+            <span className="date">{story.date}</span>
+          </Card.Meta>
+          <Card.Description style={{ fontSize: '1em' }} className="description">
+            {this._truncateDescription(story.description)}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <span>
+            <Icon size="large" name="user" />
+          </span>
+          <span>
+            <Icon size="large" name="trash" />
+          </span>
+        </Card.Content>
+      </Card>
+    );
+  }
+}
 
+export default connect(null)(ContentCard);
