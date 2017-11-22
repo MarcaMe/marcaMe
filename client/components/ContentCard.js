f0fef2f8d55239d23a9a3e343a7fe9c5e4c6ae1c
Sol <sppark95@hotmail.com>
2017-11-22 13:20:29 -0500

    chore(lint)-fix lint issues

--- client/components/ContentCard.js
+++ client/components/ContentCard.js
@@ -2,29 +2,42 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Card, Icon, Image } from 'semantic-ui-react';
 
-
-export const ContentCard = (props) => {
-    return (
-    <Card style={{width: "300px", height: "350px", margin: "0.5vw"}} color={props.color} className="card" fluid> 
-        <Card.Content style={{overflow: "hidden"}}>
-            <Card.Header>{props.story.title}</Card.Header>
-            <div style={{margin:"10px auto", overflow: "hidden", height:"150px"}}>
-              <Image style={{width: "100%", display: "block"}} fluid src={props.story.imageUrl} />
-             </div>
-            <Card.Meta>
-                <span className="date">{props.story.date}</span>
-            </Card.Meta>
-            <Card.Description style={{"fontSize": "1em"}} className="description">{truncateDescription(props.story.description)}</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-            <Icon name="user">placeholder</Icon>
-        </Card.Content>
-    </Card>
-      )
-}
-
 const truncateDescription = des => {
-  const desArr = des.split(" ");
-  if (desArr.length > 19) return desArr.slice(0,19).join(" ") + "..."
-  else return des
-}
+  const desArr = des.split(' ');
+  if (desArr.length > 19) return desArr.slice(0, 19).join(' ') + '...';
+  else return des;
+};
+
+export const ContentCard = props => {
+  return (
+    <Card
+      style={{ width: '300px', height: '350px', margin: '0.5vw' }}
+      color={props.color}
+      className="card"
+      fluid
+    >
+      <Card.Content style={{ overflow: 'hidden' }}>
+        <Card.Header>{props.story.title}</Card.Header>
+        <div
+          style={{ margin: '10px auto', overflow: 'hidden', height: '150px' }}
+        >
+          <Image
+            style={{ width: '100%', display: 'block' }}
+            fluid
+            src={props.story.imageUrl}
+          />
+        </div>
+        <Card.Meta>
+          <span className="date">{props.story.date}</span>
+        </Card.Meta>
+        <Card.Description style={{ fontSize: '1em' }} className="description">
+          {truncateDescription(props.story.description)}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Icon name="user">placeholder</Icon>
+      </Card.Content>
+    </Card>
+  );
+};
+
