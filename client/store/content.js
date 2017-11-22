757fe0e42a5a0eeab8dd6745817dca343f7eb411
Sol <sppark95@hotmail.com>
2017-11-21 20:25:28 -0500

    feat(ViewAllContent)- store, dispatch, thunk setup for fetching all content

--- client/store/content.js
+++ client/store/content.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+const GET_ALL_CONTENT = 'GET_ALL_CONTENT';
 const ADD_CONTENT = 'ADD_CONTENT';
 const GET_SINGLE_CONTENT = 'GET_SINGLE_CONTENT';
 
@@ -7,6 +8,14 @@ const defaultContent = [];
 
 const addContent = content => ({ type: ADD_CONTENT, content });
 const getContent = content => ({ type: GET_SINGLE_CONTENT, content });
+const getAllContent = content => ({ type: GET_ALL_CONTENT, content });
+
+export const fetchAllContent = () => dispatch => {
+  axios
+    .get('/api/contents')
+    .then(content => dispatch(getAllContent(content)))
+    .catch(err => console.error(err));
+};
 
 export const postContentThunk = contentBody => dispatch => {
   axios
@@ -16,19 +25,21 @@ export const postContentThunk = contentBody => dispatch => {
 };
 
 export const getSingleContent = contentId => dispatch => {
-  axios.get(`/api/contents/${contentId}`)
-  .then(res => res.data)
-  .then(content => dispatch(getContent(content)))
-  .catch(err => console.error(err));
-}
-
+  axios
+    .get(`/api/contents/${contentId}`)
+    .then(res => res.data)
+    .then(content => dispatch(getContent(content)))
+    .catch(err => console.error(err));
+};
 
 export default function(state = defaultContent, action) {
   switch (action.type) {
+    case GET_ALL_CONTENT:
+      return action.content
     case ADD_CONTENT:
       return action.content;
     case GET_SINGLE_CONTENT:
-      return [ action.content ];
+      return [action.content];
     default:
       return state;
   }
