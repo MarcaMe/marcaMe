791e6338be586c17cdb53fa2f172f1c1b90439c3
Sol <sppark95@hotmail.com>
2017-11-23 11:03:32 -0500

    feat(contentstore)-create dispatch, actioncreator for editing content in content store

--- client/store/content.js
+++ client/store/content.js
@@ -4,6 +4,7 @@ const GET_ALL_CONTENT = 'GET_ALL_CONTENT';
 const ADD_CONTENT = 'ADD_CONTENT';
 const GET_SINGLE_CONTENT = 'GET_SINGLE_CONTENT';
 const DELETE_SINGLE_CONTENT = 'DELETE_SINGLE_CONTENT';
+const EDIT_SINGLE_CONTENT = 'EDIT_SINGLE_CONTENT';
 
 const defaultContent = [];
 
@@ -11,6 +12,7 @@ const addContent = content => ({ type: ADD_CONTENT, content });
 const getContent = content => ({ type: GET_SINGLE_CONTENT, content });
 const getAllContent = content => ({ type: GET_ALL_CONTENT, content });
 const deleteSingleContent = contentId => ({ type: DELETE_SINGLE_CONTENT, contentId })
+const editSingleContent = content => ({ type: EDIT_SINGLE_CONTENT, content })
 
 export const fetchAllContent = () => dispatch => {
   axios
@@ -44,6 +46,13 @@ export const deleteOneContent = (contentId, history) => dispatch => {
   .catch(err => console.error(err))
 }
 
+export const editOneContent = contentBody => dispatch => {
+  axios.put(`/api/contents/${contentBody.id}`, contentBody)
+  .then(res => res.data)
+  .then(content => dispatch(editSingleContent(content)))
+  .catch(err => console.error(err));
+}
+
 export default function(state = defaultContent, action) {
   switch (action.type) {
     case GET_ALL_CONTENT:
@@ -52,6 +61,8 @@ export default function(state = defaultContent, action) {
       return [...state, action.content];
     case GET_SINGLE_CONTENT:
       return [action.content];
+    case EDIT_SINGLE_CONTENT:
+      return state.map(content => ((content.id === action.content.id) ? action.content : content))
     case DELETE_SINGLE_CONTENT:
       return state.filter(content => (+content.id !== +action.contentId))
     default:
