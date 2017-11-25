b597e97b8c6ce4a750924708d8f911f7ad477ab3
Kenny Diaz <kend77@gmail.com>
2017-11-24 22:55:16 -0500

    fix(oneArticle): Add link that opens in new tab instead of replacing current article

--- client/components/OneArticle.js
+++ client/components/OneArticle.js
@@ -4,6 +4,7 @@ import { getSingleContent } from '../store/content';
 import ReactHtmlParser from 'react-html-parser';
 import { Container, Sticky } from 'semantic-ui-react';
 import ReaderNav from './ReaderNav';
+import { Link } from 'react-router-dom'
 
 class OneArticle extends React.Component {
   componentDidMount() {
@@ -22,10 +23,10 @@ class OneArticle extends React.Component {
             <h5>{`Saved on ${Date(article.createdAt)}`}</h5>
             <div className="header-container">
               <h3>{article.author && `By ${article.author}`}</h3>
-              <a href={article.url}>
+              <Link href={article.url}>
                 {' '}
                 <h3>Go to original </h3>
-              </a>
+              </>
             </div>
           </Container>
           <Container text>{ReactHtmlParser(article.content)}</Container>
