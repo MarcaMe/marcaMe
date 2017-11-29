4c6ee345e6939c5724939eafc46f2976b74bae83
Sisi Qin <xixiqinms@gmail.com>
2017-11-29 16:13:21 -0500

    feat(social)-client get article notification functional

--- client/components/Notification.js
+++ client/components/Notification.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Icon, Button } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 import { FullCard } from '../components';
+import { editOneContent } from '../store';
 
 class Notification extends Component {
   constructor(props) {
@@ -11,7 +12,16 @@ class Notification extends Component {
 
     }
   }
-
+  componentWillUnmount(){
+    const newContent = this.props.content.filter( article => article.isNew === true && article.sharedFrom > 0 );
+    const updatedContent = newContent.map( function(oneContent){
+      Object.assign(oneContent);
+      oneContent.isNew = false
+      return oneContent
+    })
+    updatedContent.map(oneContent => this.props.markAsOld(oneContent))
+  }
+  
   render() {
     const newContent = this.props.content.filter( article => article.isNew === true && article.sharedFrom > 0 );
     return (
@@ -26,5 +36,12 @@ const mapState = state => ({
   following: state.following
 })
 
+const mapDispatch = dispatch => {
+  return {
+    markAsOld(id, old){
+      dispatch(editOneContent(id, old))
+    }
+  }
+}
 
-export default connect(mapState)(Notification);
+export default connect(mapState, mapDispatch)(Notification);
